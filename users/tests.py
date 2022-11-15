e70a2d11daa462f1a1844de0adcabbf0da2827cb
som2145 <som2145@gmail.com>
2022-11-15 10:52:05 +0900

    test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from users.models import User
 # Create your tests here.
+class UserResgistrationTest(APITestCase):
+    def test_registration(self):
+        url = reverse("user_view") #urls.py 에서 지정해 준 url의 name
+        user_data = {
+            "username" : "testuser",
+            "fullname" : "테스터",
+            "email" : "test@testuser.com",
+            "password" : "password",
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.data["message"], "가입 완료!!")
